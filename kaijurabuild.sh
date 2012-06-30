e7b12c80f3b8fb1cb373a61fee6daa0cc3e9e620
kaijura <emailkaijura@gmail.com>
2012-06-30 13:23:52 -0700

    change toolchain

--- kaijurabuild.sh
+++ kaijurabuild.sh
@@ -1,2 +1,3 @@
-export CROSS_COMPILE=/home/kaijura/cmkernel/arm.eabi/bin/arm-none-eabi-
+export CROSS_COMPILE=/home/vic/cmkernel/android-ndk-r8-toolchain/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-
+export ARCH=arm
 make EXTRA_AFLAGS=-mfpu=neon
