5e2361cfbf652a06042b67c52b38d663e77010f4
kaijura <emailkaijura@gmail.com>
2012-07-02 14:33:20 -0700

    buildconf: try different toolchain

--- kaijurabuild.sh
+++ kaijurabuild.sh
@@ -1,3 +1,4 @@
-export CROSS_COMPILE=/home/vic/cmkernel/android-ndk-r8-toolchain/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-
+#export CROSS_COMPILE=/home/vic/cmkernel/android-ndk-r8-toolchain/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/vic/cmkernel/arm.eabi/bin/arm-none-eabi-
 export ARCH=arm
-make EXTRA_AFLAGS=-mfpu=neon
+make -j2 EXTRA_AFLAGS=-mfpu=neon > /home/vic/cmkernel/lastbuild.log
